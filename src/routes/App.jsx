5dfaceaa1ef98aa567b15093652f9a7998229885
Matias Barril <matibarril@gmail.com>
2022-11-10 13:23:36 -0300

    react structure components

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Layout from "../containers/Layout";
-import Login from  "../containers/Login";
-import RecoveryPassword from "../containers/RecoveryPassword";
+import Login from  "../pages/Login";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
+import PasswordRecovery from '../pages/PasswordRecovery';
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAccount';
+import CreateAccount from '../pages/CreateAccount';
+import Checkout from '../pages/Checkout';
+import Orders from '../pages/Orders';
 import '../styles/global.css';
 
 const App = () => {
@@ -15,7 +21,13 @@ const App = () => {
                 <Routes>
                     <Route exact path="/" element={<Home/>}/>
                     <Route exact path="/login" element={<Login/>}/>
-                    <Route exact path="/recovery-password" element={<RecoveryPassword/>}/>
+                    <Route exact path="/password-recovery" element={<PasswordRecovery/>}/>
+                    <Route exact path="/send-email" element={<SendEmail/>} />
+					<Route exact path="/new-password" element={<NewPassword/>} />
+					<Route exact path="/account" element={<MyAccount/>} />
+					<Route exact path="/signup" element={<CreateAccount/>} />
+					<Route exact path="/checkout" element={<Checkout/>} />
+					<Route exact path="/orders" element={<Orders/>} />
                     <Route path="*" element={<NotFound/>}/>
                 </Routes>    
             </Layout>
